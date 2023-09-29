28a6d4719fe1ae4988f2c89e3486eb12556a963b
bansodevaibhav032 <129043443+bansodevaibhav032@users.noreply.github.com>
2023-09-29 20:26:27 +0530

    add functionality and css styling

--- s1.js
+++ s1.js
@@ -1,2 +1,15 @@
-
+function updateClock() {
+    const now = new Date();
+    let hours = now.getHours();
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+    const meridiem = hours >= 12 ? 'PM' : 'AM';
+      
+    hours = hours % 12 || 12;
+  
+    const timeString = `${hours}:${minutes}:${seconds} ${meridiem}`;
+    document.getElementById('clock').textContent = timeString;
+  }  
+  setInterval(updateClock, 1000);
+  updateClock();
   
